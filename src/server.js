43348694cd18bc77cd3c1f8b204dc4fb84c560c1
Hieu Tran Trung <hieutrantrung@Hieus-MacBook-Pro-2.local>
2018-11-15 22:34:06 +0700

    allow origin

--- src/server.js
+++ src/server.js
@@ -6,11 +6,26 @@ import compression from 'compression';
 import config from './config';
 import logger from './utils/logger';
 
-// require('./utils/db');
-
 const api = express();
 
-api.use('*', cors());
+api.use((req, res, next) => {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 api.use(compression());
 api.use(bodyParser.urlencoded({ extended: true }));
 api.use(bodyParser.json());
