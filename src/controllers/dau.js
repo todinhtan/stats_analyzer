b62b0808c3e7222a87f9f85adb5350bf62a7c77c
Tan <tan@intersquad.co>
2018-11-15 21:05:20 +0700

    add GET /dau/:vendorId to download DAU of single vendor

--- src/controllers/dau.js
+++ src/controllers/dau.js
@@ -1,5 +1,6 @@
 import { MongoClient } from 'mongodb';
 import httpStatus from 'http-status-codes';
+import json2csv from 'json2csv';
 import logger from '../utils/logger';
 import config from '../config';
 import dateUtils from '../utils/date';
@@ -100,7 +101,79 @@ export async function get(req, res) {
 
     // close connection
     connection.close();
+    // return 404 http not found if no documents found
+    if (!docs || docs.length === 0) return res.status(httpStatus.NOT_FOUND).json('Not found').end();
     return res.json(docs).end();
+  } catch (error) {
+    logger.error(error);
+    return res.status(httpStatus.INTERNAL_SERVER_ERROR).end();
+  }
+}
+
+export async function getSingle(req, res) {
+  const startDateQuery = req.query.start_date;
+  const endDateQuery = req.query.end_date;
+  const { vendorId } = req.params;
+
+  // require on start and end date
+  if (!startDateQuery || !endDateQuery) return res.status(httpStatus.NOT_FOUND).json({ message: 'start_date or end_date is missing !' }).end();
+
+  const startDate = new Date(startDateQuery);
+  const endDate = new Date(endDateQuery);
+
+  if (!dateUtils.isValidDate(startDate) || !dateUtils.isValidDate(endDate)) return res.status(httpStatus.BAD_REQUEST).json({ message: 'start_date or end_date is not valid date' }).end();
+
+  // connect mongodb
+  const connection = await MongoClient.connect(config.database.uri, { useNewUrlParser: true })
+    .catch(err => logger.error(err));
+  // throw http 500 if connect failed
+  if (!connection) return res.status(httpStatus.INTERNAL_SERVER_ERROR).end();
+
+  try {
+    const collection = connection.db().collection('dau');
+    const docs = await collection.aggregate([
+      {
+        $match: {
+          $and: [
+            { vendorId },
+            { time: { $gte: startDate } },
+            { time: { $lte: endDate } },
+          ],
+        },
+      },
+      {
+        $group: {
+          _id: {
+            vendorId: '$vendorId',
+            vendorName: '$vendorName',
+            time: '$time',
+          },
+          // vendorName: '$vendorName',
+          count: { $sum: 1 },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          date: '$_id.time',
+          count: 1,
+        },
+      },
+      { $sort: { date: 1 } },
+    ]).toArray().catch((err) => { logger.error(err); });
+
+    // close connection
+    connection.close();
+    // return 404 http not found if no documents found
+    if (!docs || docs.length === 0) return res.status(httpStatus.NOT_FOUND).json('Not found').end();
+
+    const fields = ['date', 'count'];
+    const json2csvParser = new json2csv.Parser({ fields });
+    const csv = json2csvParser.parse(docs);
+
+    res.setHeader('Content-Disposition', `attachment; filename=dau_${vendorId}.csv`);
+    res.type('text/csv');
+    res.send(csv).end();
   } catch (error) {
     logger.error(error);
     return res.status(httpStatus.INTERNAL_SERVER_ERROR).end();
--- src/routes/dau.js
+++ src/routes/dau.js
@@ -1,6 +1,7 @@
-import { mockup, get } from '../controllers/dau';
+import { get, getSingle } from '../controllers/dau';
 
 export default (api) => {
-  api.route('/dau/mockup').get(mockup);
+  // api.route('/dau/mockup').get(mockup);
   api.route('/dau').get(get);
+  api.route('/dau/:vendorId').get(getSingle);
 };
